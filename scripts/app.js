6199520e8c50e1f6db9fb042da5732f54a3e3a82
samorah47 <adebanjoajibola9@gmail.com>
2024-06-11 19:39:08 -0400

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+
+const studentName = 'Ajibola';
+const studentNumber = 1131736;
+const result = `${ studentName} - ${ studentNumber}`;
+console.log(result);
+
+const header = document.querySelector('h1');
+header.innerHTML = 'Adebanjo Ajibola Samuel - 1131736';
+header.classList.add('heading1');
+
+
+console.log(header);
